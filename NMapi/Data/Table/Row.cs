caf4ba87474b38ec919792c130a670fb4b61fc91
Johannes Roith <johannes.roith@topalis.com>
2009-03-05 22:24:06 +0000

    *sigh*
    git-svn-id: http://svn.openmapi.org/svn/openmapi/trunk/nmapi@89 7e9c2147-6d2a-4f89-8ef6-0215227d3333

---
+++ NMapi/Data/Table/Row.cs
@@ -0,0 +1,133 @@
+//
+// openmapi.org - NMapi C# Mapi API - SRow.cs
+//
+// Copyright 2008 VipCom AG
+//
+// Author (Javajumapi): VipCOM AG
+// Author (C# port):    Johannes Roith <johannes@jroith.de>
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+//
+
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+using System.Diagnostics;
+using CompactTeaSharp;
+
+
+using NMapi;
+using NMapi.Flags;
+using NMapi.Events;
+using NMapi.Properties;
+using NMapi.Table;
+using NMapi.Interop;
+
+namespace NMapi.Table {
+
+	/// <summary>
+	///  The SRow structure.
+	/// </summary>
+	/// <remarks>
+	///  See MSDN: http://msdn2.microsoft.com/en-us/library/ms526769.aspx
+	/// </remarks>
+
+	[DataContract (Namespace="http://schemas.openmapi.org/indigo/1.0")]
+	public class Row : IXdrAble
+	{	
+		public PropertyValue[] lpProps;
+
+		public Row () 
+		{
+		}
+	
+		public Row (PropertyValue [] values)
+		{
+			lpProps = values;
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		[DataMember (Name="Props")]
+		public PropertyValue [] Props
+		{
+			get { return lpProps; }
+			set { lpProps = value; }
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <remarks>
+		///  This method is unique to NMapi.
+		/// </remarks>
+		public PropertyValue FindProperty (int propertyName)
+		{
+			int index = FindPropertyIndex (propertyName);
+			if (index > 0)
+				return lpProps [index];
+			return null;
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <remarks>
+		///  This method is unique to NMapi.
+		/// </remarks>
+    	public int FindPropertyIndex (int propertyName)
+		{
+			if (lpProps == null)
+				return -1;
+			int i = 0;
+			foreach (PropertyValue prop in lpProps) {
+				if (prop.PropTag == propertyName)
+					return i;
+				i++;
+			}
+			return -1;
+		}
+
+		internal Row (XdrDecodingStream xdr)
+		{
+			XdrDecode (xdr);
+		}
+
+		[Obsolete]
+		public void XdrEncode (XdrEncodingStream xdr)
+		{
+			Trace.WriteLine ("XdrEncode called: " + this.GetType ().Name);
+			int size = lpProps.Length;
+			xdr.XdrEncodeInt (size);
+			for (int i = 0; i < size; i++)
+				lpProps [i].XdrEncode (xdr);
+		}
+
+		[Obsolete]
+		public void XdrDecode (XdrDecodingStream xdr)
+		{
+			Trace.WriteLine ("XdrDecode called: " + this.GetType ().Name);
+			int size = xdr.XdrDecodeInt ();
+			lpProps = new PropertyValue [size];
+			for (int i = 0; i < size; i++)
+				lpProps [i] = PropertyValue.Decode (xdr);
+		}
+	
+	}
+
+}
---
+++ NMapi/Data/Table/RowSet.cs
@@ -0,0 +1,116 @@
+//
+// openmapi.org - NMapi C# Mapi API - RowSet.cs
+//
+// Copyright 2008 VipCom AG
+//
+// Author (Javajumapi): VipCOM AG
+// Author (C# port):    Johannes Roith <johannes@jroith.de>
+//
+// This is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of
+// the License, or (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this software; if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+using NMapi;
+using NMapi.Flags;
+using NMapi.Events;
+using NMapi.Properties;
+using NMapi.Table;
+using NMapi.Interop;
+
+namespace NMapi.Table {
+
+	/// <summary>
+	///  The RowSet structure.
+	/// </summary>
+	/// <remarks>
+	///  See MSDN: http://msdn2.microsoft.com/en-us/library/ms527417.aspx
+	/// </remarks>
+
+	[DataContract (Namespace="http://schemas.openmapi.org/indigo/1.0")]
+	public class RowSet : IEnumerable<Row>
+	{
+		private Row [] aRow;
+	    
+		/// <summary>
+		///   
+		/// </summary>
+		[DataMember (Name="ARow")]
+		public Row [] ARow {
+			get { return aRow; }
+			set { aRow = value; }
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <remarks>
+		///  This property is unique to NMapi.
+		/// </remarks>
+		public int Count {
+			get {
+				if (aRow == null)
+					return 0;
+				return aRow.Length;
+			}
+		}
+
+		//  
+		// provide some proxy access for aRow ...
+		//
+
+		public Row this [int index]
+		{
+			get {
+				if (aRow == null)
+					return null;
+				return aRow [index];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return GetEnumerator();
+		}
+
+		public IEnumerator<Row> GetEnumerator ()
+		{
+			if (aRow == null)
+				yield break;
+			foreach (Row row in aRow)
+				yield return row;
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		public RowSet ()
+		{
+		}
+
+		/// <summary>
+		///
+		/// </summary>		
+		public RowSet (Row [] values)
+		{
+			aRow = values;
+		}
+	}
+
+}
