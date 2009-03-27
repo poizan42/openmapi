7d68b223829c50bfd580bbd781a5e138e98beb8e
Michael Kukat <michael.kukat@to.com>
2009-03-27 12:45:28 +0100

    Fixed renaming/moving of folders

--- gateways/imap/commands/CmdRename.cs
+++ gateways/imap/commands/CmdRename.cs
@@ -3,6 +3,7 @@
 // Copyright 2008 Topalis AG
 //
 // Author: Andreas Huegel <andreas.huegel@topalis.com>
+//         Michael Kukat <michael.kukat@to.com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -46,36 +47,66 @@ namespace NMapi.Gateways.IMAP {
 		public override void Run (Command command)
 		{
 			try {
+				// check arguments
+				if (command.Mailbox1 == command.Mailbox2) {
+					state.ResponseManager.AddResponse (
+						new Response (ResponseState.NO, Name, command.Tag).AddResponseItem ("destination name has to differ from source name"));
+					return;
+				}
 
+				// check if folder is mapped
 				if (state.FolderMappingAgent.MapiFolderIsMapped (ConversionHelper.MailboxIMAPToUnicode (command.Mailbox1))) {
 					state.ResponseManager.AddResponse (
 						new Response (ResponseState.NO, Name, command.Tag).AddResponseItem ("folder may not be renamed"));
 					return;
 				}
 
-				string path = PathHelper.ResolveAbsolutePath (PathHelper.PathSeparator + ConversionHelper.MailboxIMAPToUnicode (command.Mailbox1));
-				IMapiFolder folder = ServCon.OpenFolder (path);
-				if (folder == null) {
+				// calculate all necessary path elements
+				string srcPath = PathHelper.ResolveAbsolutePath (PathHelper.PathSeparator + ConversionHelper.MailboxIMAPToUnicode (command.Mailbox1));
+				string srcParentPath = PathHelper.GetParent (srcPath);
+				string srcFolderName = PathHelper.GetLast (srcPath);
+
+				string destPath = PathHelper.ResolveAbsolutePath (PathHelper.PathSeparator + ConversionHelper.MailboxIMAPToUnicode (command.Mailbox2));
+				string destParentPath = PathHelper.GetParent (destPath);
+				string destFolderName = PathHelper.GetLast (destPath);
+
+				// try to open folders
+				IMapiFolder destFolder = null;
+				destFolder = ServCon.OpenFolder (destParentPath);
+
+				IMapiFolder parent = null;
+				parent = ServCon.OpenFolder (srcPath);
+
+				SBinary entryId = null;
+
+				// if opening folders was successful, do the rename/move
+				if(parent != null && destFolder != null) {
+					// get entry ID
+					var prop = new MapiPropHelper (parent).HrGetOneProp (Property.EntryId);
+					entryId = ((BinaryProperty) prop).Value;
+
+					// close parent (which in fact was source) and open source parent
+					parent.Close ();
+					parent = ServCon.OpenFolder (srcParentPath);
+
+					// move folder to destination, use unicode-flag to make sure the name doesn't get garbled
+					parent.CopyFolder (entryId.ByteArray, null, destFolder, destFolderName, null, NMAPI.FOLDER_MOVE | Mapi.Unicode);
+
+					// if we came here, return success
+					state.ResponseManager.AddResponse (new Response (ResponseState.OK, Name, command.Tag));
+				} else {
+					// one of the folders wasn't a folder
 					state.ResponseManager.AddResponse (
-						new Response (ResponseState.NO, Name, command.Tag).AddResponseItem ("given folder does not exist"));
+						new Response (ResponseState.NO, Name, command.Tag).AddResponseItem ("at least one of the arguments is not a folder"));
 					return;
 				}
 
-				string nameNew = PathHelper.Path2Array(ConversionHelper.MailboxIMAPToUnicode (command.Mailbox2)).Last ();
-				UnicodeProperty nameProp = new UnicodeProperty ();
-				nameProp.PropTag = Property.DisplayNameW;
-				nameProp.Value = nameNew;
-				
-				MapiPropHelper mph = new MapiPropHelper (folder);
-				mph.HrSetOneProp (nameProp);
-				folder.SaveChanges (NMAPI.FORCE_SAVE);
-				state.ResponseManager.AddResponse (new Response (ResponseState.OK, Name, command.Tag));
 			}
 			catch (Exception e) {
+				// XXX better error handling would be nice
 				state.ResponseManager.AddResponse (new Response (ResponseState.NO, Name, command.Tag).AddResponseItem (e.Message, ResponseItemMode.ForceAtom));
 				state.Log (e.StackTrace);
 			}
 		}
-
 	}
 }
